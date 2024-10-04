7e2744972196301c463e0fdc902d9b23fa999e50
RuanMSantos <ruancauancauan@gmail.com>
2024-10-03 22:40:12 -0300

    Adiciona lógica de não repetir a escolha da máquina

--- Program.cs
+++ Program.cs
@@ -1,13 +1,11 @@
-﻿int numeroPlayer = 0;
-string resposta = "";
-int pontuacaoMaquina = 0;
-int pontuacaoPlayer = 0;
-int NumeroMaquina = 0;
-string vencedor = "";
+﻿string resposta = "", vencedor = "";
+int numeroPlayer = 0, numeroMaquina = 0, pontuacaoPlayer = 0, pontuacaoMaquina = 0; 
+int teste = 0, rodada = 0;
+int dificuldade = 1, respostaDificuldade = 0;
  
 Loop();
 void Loop(){
- 
+    Dificuldade();
     Tela();
     Maquina();
     Comparador();
@@ -23,8 +21,11 @@ void Loop(){
         vencedor = "Você perdeu";
     }
 }
+
 Console.WriteLine($"{vencedor}");
+
 void Tela(){
+    rodada++;
     Pontuacao();
     Console.WriteLine("0. Pedra 1. Papel 2. Tesoura");
     resposta = Console.ReadLine()!;
@@ -39,38 +40,45 @@ void Tela(){
         Tela();
     }
 }
+
 void Maquina(){
     int random = new Random().Next(0, 3);
-    NumeroMaquina = random;
+    teste = random;
+    if (rodada >= 2){
+        if (teste == numeroMaquina){
+            Maquina();
+        }
+    }
+    numeroMaquina = teste;
 }
-void Comparador()
-{
-    if (numeroPlayer == 0 && NumeroMaquina == 1){
+
+void Comparador(){
+    if (numeroPlayer == 0 && numeroMaquina == 1){
         pontuacaoMaquina++;
         Console.WriteLine("\nPonto da máquina");
         Thread.Sleep(500);
     }
-    else if (numeroPlayer == 0 && NumeroMaquina == 2){
+    else if (numeroPlayer == 0 && numeroMaquina == 2){
         pontuacaoPlayer++;
         Console.WriteLine("\nPonto do jogador");
         Thread.Sleep(500);
     }
-    else if (numeroPlayer == 1 && NumeroMaquina == 0){
+    else if (numeroPlayer == 1 && numeroMaquina == 0){
         pontuacaoPlayer++;
         Console.WriteLine("\nPonto do jogador");
         Thread.Sleep(500);
     }
-    else if (numeroPlayer == 1 && NumeroMaquina == 2){
+    else if (numeroPlayer == 1 && numeroMaquina == 2){
         pontuacaoMaquina++;
         Console.WriteLine("\nPonto da máquina");
         Thread.Sleep(500);
     }
-    else if (numeroPlayer == 2 && NumeroMaquina == 0){
+    else if (numeroPlayer == 2 && numeroMaquina == 0){
         pontuacaoMaquina++;
         Console.WriteLine("\nPonto da máquina");
         Thread.Sleep(500);
     }
-    else if (numeroPlayer == 2 && NumeroMaquina == 1){
+    else if (numeroPlayer == 2 && numeroMaquina == 1){
         pontuacaoPlayer++;
         Console.WriteLine("\nPonto do jogador");
         Thread.Sleep(500);
@@ -80,9 +88,16 @@ void Comparador()
         Thread.Sleep(500);
     }
 }
+
 void Pontuacao(){
     Console.Clear();
     Console.WriteLine("=> Pedra - Papel - Tesoursa <=\n");
     Console.WriteLine($"Pontuação Máquina: {pontuacaoMaquina}");
     Console.WriteLine($"Pontuação Player: {pontuacaoPlayer}\n");
+}
+
+void Dificuldade(){
+    Pontuacao();
+    Console.WriteLine("Escolha a dificuldade: 1. 2.");
+    respostaDificuldade = Convert.ToInt32(Console.ReadLine());
 }
